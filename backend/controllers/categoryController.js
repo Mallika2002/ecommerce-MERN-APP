6bdf65170b30ff77f273c3056029c68ed9c69c94
Nithish Boddeda <nithish.boddeda@capgemini.com>
2023-06-25 23:05:47 +0530

    Added Swagger Support for Backend endpoints for Categories

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,5 +1,34 @@
 const Category = require('../models/CategoryModel')
 
+/**
+ * @swagger
+ * /categories:
+ *   get:
+ *     summary: Get all categories
+ *     tags: [Categories]
+ *     description: Retrieve a list of all categories
+ *     responses:
+ *       200:
+ *         description: Fetches all categories
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                   name:
+ *                     type: string
+ *                   description:
+ *                     type: string
+ *                   image:
+ *                     type: string
+ *                   attrs:
+ *                     type: string
+ *                   __v:
+ *                     type: integer
+ */
 const getCategories = async (req, res, next) => {
     // res.send("Handling category routes e.g. search for categories")
     try {
@@ -10,6 +39,44 @@ const getCategories = async (req, res, next) => {
     }
 }
 
+ /**
+ * @swagger
+ * /categories:
+ *   post:
+ *     tags: [Categories]
+ *     summary : Add a new category
+ *     requestBody:
+ *       description: JSON Object containing the category name
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *           example:
+ *             category: "Category Name"
+ *            
+ *     responses:
+ *       201:
+ *         description: Returns a confirmation message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                   name:
+ *                     type: string
+ *                   description:
+ *                     type: string
+ *                   image:
+ *                     type: string
+ *                   attrs:
+ *                     type: string
+ *                   __v:
+ *                     type: integer
+ */
 const newCategory = async (req, res, next) => {
     try {
         // res.send(!!req.body)
@@ -29,7 +96,7 @@ const newCategory = async (req, res, next) => {
             })
             res.status(201).send({categoryCreated: categoryCreated})
         }
-        res.send(category)
+        // res.send(category)
     } catch (error) {
         next(error)
     }
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,50 @@
 const express = require('express')
 const app = express()
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 const port = 5000
+
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.3",
+    info: {
+      title: 'E-commerce Application',
+      version: '1.0.0',
+      description: 'E-commerce Application API Documentation',
+      termsOfService: 'http://example.com/terms/',
+      contact: {
+        name: 'Mallika Swetha B',
+        Number: '+91 8374133625',
+        url: "https://www.mallikaswetha.com",
+        email: "mallikaswetha2002@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000/api',
+        description: 'Local server',
+      },
+      {
+        url: 'http://43.204.171.45:5000/api/',
+        description: 'Production server',
+      }
+    ],
+  },
+  // ['.routes/*.js']
+  apis: ["./controllers/*.js"],
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.use(express.json()) //middleware to parse json data from req.body
 
 const apiRoutes = require('./routes/apiRoutes')
 
 app.get('/', async (req, res, next) => {
-  res.json({ message: 'API Running..' })
-})
+    res.json({ message: 'API Running..' })
+  })
 
 
 //mongoDB connection
@@ -20,24 +56,24 @@ app.use('/api', apiRoutes)                          //if URL starts with /api re
 //it is good programming practice to sepearet different routes in different files..
 
 app.use((error, req, res, next) => {
-  if (process.env.NODE_ENV === "development") {
-    console.error(error);
-  }
-  next(error);
-});
+    if (process.env.NODE_ENV === "development") {
+      console.error(error);
+    }
+    next(error);
+  });
 
-app.use((error, req, res, next) => {
-  if (process.env.NODE_ENV === "development") {
-    res.status(500).json({
-      message: error.message,
-      stack: error.stack,
-    });
-  } else {
-    res.status(500).json({
-      message: error.message,
-    })
-  }
-});
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  app.use((error, req, res, next) => {
+    if (process.env.NODE_ENV === "development") {
+      res.status(500).json({
+        message: error.message,
+        stack: error.stack,
+      });
+    } else {
+      res.status(500).json({
+        message: error.message,
+      })
+    }
+  });
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
